0b86c66a7e30c2b2302942ec5d9ccced46de160e
harekishore <62699671+harekishore@users.noreply.github.com>
2024-08-22 20:44:49 +0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,1 +1,2 @@
-importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
+/*! For license information please see serviceworker.js.LICENSE.txt */
+importScripts("https://cdn.moengage.com/release/dc_1/serviceworker_cdn.min.latest.js");
